f2538e00301a54ae8dceea2783b3d2cdd43412a3
shafqaat <rajashajaatali@gmail.com>
2025-04-22 02:50:00 +0500

    don't know

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -33,10 +33,11 @@ const FooterLinks = styled.ul`
 const FooterLi = styled.li`
     white-space: nowrap;
     width: fit-content;
-    padding: 3px;
+    padding:0 15px;
     transition: background-color 0.5s;
     &:hover{
       background-color: rgba(66, 61, 61, 0.5);
+      border-radius: 5px;
     }
 `
 
@@ -44,6 +45,7 @@ const FooterIcons = styled.div`
     display: flex;
     gap: 20px;
     margin: 40px 0;
+    /* border: 2px solid gray; */
 
     img{
 
